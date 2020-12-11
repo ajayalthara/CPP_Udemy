17f26a6b9f535520b928a74346a2cdbf964237a8
Ajay AJ <ajayalthara@gmail.com>
2020-12-11 01:24:44 -0800

    Use of constants. Carpet cleaning estimate

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ Project1 - Debug ]----------"
-	@cd "Project1" && "$(MAKE)" -f  "Project1.mk"
+	@echo "----------Building project:[ CarpetCleaningEstimate - Debug ]----------"
+	@cd "CarpetCleaningEstimate" && "$(MAKE)" -f  "CarpetCleaningEstimate.mk"
 clean:
-	@echo "----------Cleaning project:[ Project1 - Debug ]----------"
-	@cd "Project1" && "$(MAKE)" -f  "Project1.mk" clean
+	@echo "----------Cleaning project:[ CarpetCleaningEstimate - Debug ]----------"
+	@cd "CarpetCleaningEstimate" && "$(MAKE)" -f  "CarpetCleaningEstimate.mk" clean
--- Project2/main.cpp
+++ Project2/main.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 
 int main() {
-    int favorite_no;
-    cout << "Enter your favorite number \t";
-    cin >>  favorite_no;
-    cout << favorite_no << " is my facorite number too" << std::endl;
+//    int favorite_no;
+//    cout << "Enter your favorite number \t";
+//    cin >>  favorite_no;
+//    cout << favorite_no << " is my facorite number too" << std::endl;
+//    
+//    int var1, var2;
+//    cout << "Enter first number ";
+//    // Try entering 2 numbers separated by a space. cin for var2 will read the second number after space and will not wait for enter a second time
+//    cin >> var1;
+//    cout << "Enter second number ";
+//    cin >> var2;
+//    cout << "Numbers are "<< var1 << " and "<< var2;
+//    
+    bool myBoolVar {true};
+    cout << "The bool value is " << myBoolVar << endl;
     
-    int var1, var2;
-    cout << "Enter first number ";
-    // Try entering 2 numbers separated by a space. cin for var2 will read the second number after space and will not wait for enter a second time
-    cin >> var1;
-    cout << "Enter second number ";
-    cin >> var2;
-    cout << "Numbers are "<< var1 << " and "<< var2;
+    char myCharVar {'L'};
+    cout << "Character value is " << myCharVar << endl;
+    
+    cout << "The mem size of variable is " << sizeof(myCharVar) << endl;
     
     return 0;
 }
